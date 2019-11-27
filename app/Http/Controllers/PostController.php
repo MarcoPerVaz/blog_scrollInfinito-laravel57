391680781b6a6fa1a8747e3b9bc811e7ce93ce4c
Curso Git <marcooperdomo@gmail.com>
2019-11-27 10:50:36 -0600

    Concepto de API

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -14,72 +14,9 @@ class PostController extends Controller
      */
     public function index()
     {
-        //
+        
+        return Post::orderBy( 'id', 'DESC' )->paginate();
+        
     }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(Request $request)
-    {
-        //
-    }
-
-    /**
-     * Display the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function show(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function edit(Post $post)
-    {
-        //
-    }
-
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function update(Request $request, Post $post)
-    {
-        //
-    }
-
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  \App\Post  $post
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy(Post $post)
-    {
-        //
-    }
 }
--- database/migrations/2019_11_27_161107_create_posts_table.php
+++ database/migrations/2019_11_27_161107_create_posts_table.php
@@ -15,6 +15,12 @@ class CreatePostsTable extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->increments('id');
+            /*  */
+                $table->string( 'title' );
+                $table->string( 'slug' );
+                $table->text( 'excerpt' );
+                $table->text( 'body' );
+            /*  */
             $table->timestamps();
         });
     }
