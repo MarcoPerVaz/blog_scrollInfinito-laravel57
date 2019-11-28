54cb49d89c7e5f21cfe536544ee375a3b21457d1
Curso Git <marcooperdomo@gmail.com>
2019-11-27 19:24:56 -0600

    Componente Vuejs con Bootstrap 4

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,10 +4,21 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
+        {{-- CSS --}}
+        <link rel="stylesheet" href="{{ asset( 'css/app.css' ) }}">    
+
         <title>Blog | Scroll Infinito</title>
 
     </head>
     <body>
-        
+
+        <div id="app">
+            {{-- Componente --}}
+            <posts></posts>
+        </div>
+
+        {{-- Scripts --}}
+        <script src="{{ asset( 'js/app.js' ) }}"></script>        
+
     </body>
 </html>
